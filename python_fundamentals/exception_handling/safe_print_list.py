c6cda3f00b484b25d9aae778c504e12c6935b260
DenZzz1 <m2nky2fficiel@gmail.com>
2026-08-06 14:34:10 +0000

    task 0

---
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def safe_print_list(my_list=[], x=0):
+    """
+    Prints x elements of a list and handles potential IndexErrors.
+
+    Returns:
+        The real number of elements printed.
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return count
