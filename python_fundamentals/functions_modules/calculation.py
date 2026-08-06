5a615b153fbf3e3aff7901b5f535aaab9f2182fe
DenZzz1 <m2nky2fficiel@gmail.com>
2026-08-06 11:38:11 +0000

    task 6

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""Module that imports calculator functions and prints results for a, b."""
 from calculator_1 import add, sub, mul, div
-
 
 if __name__ == "__main__":
     a = 10
     b = 5
+
+    # Addition
     print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Subtraction
     print("{} - {} = {}".format(a, b, sub(a, b)))
+
+    # Multiplication
     print("{} * {} = {}".format(a, b, mul(a, b)))
+
+    # Division
     print("{} / {} = {}".format(a, b, div(a, b)))
