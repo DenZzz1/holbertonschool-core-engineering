c1d6f83941744b1aa85e4fd631f444361049c209
DenZzz1 <m2nky2fficiel@gmail.com>
2026-08-09 17:22:30 +0000

    task 2

---
+++ python_advanced/file_handling/append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that defines a function to append text to a file."""
+
+
+def append_write(filename="", text=""):
+    """Append a string to a text file (UTF8) and return chars added."""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
